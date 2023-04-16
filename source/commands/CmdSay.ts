ed67c6c207d37112a860fdf5b94c7e9b615fbb62
hvlxh <monsta22019@gmail.com>
2023-04-16 18:44:30 +0530

    Command: "Say" Command

---
+++ source/commands/CmdSay.ts
@@ -0,0 +1,34 @@
+import Player from "../api/Player";
+import { Command } from "../api/commands/Base";
+import Colors from "../utils/Colors";
+
+class CommandSay extends Command {
+  constructor() {
+    super({
+      name: "say",
+      description: "Broadcast/Say a message",
+      aliases: ["broadcast"],
+      arguments: [
+        [
+          {
+            name: "message",
+            type: "string",
+            optional: false,
+          },
+        ],
+        {
+          min: 1,
+          max: 1,
+        },
+      ],
+    });
+  }
+
+  run(player: Player | null, parameters: string[]): void {
+    const message = parameters[0];
+
+    this.api.getServer().broadcast(Colors.yellow(`[Server] ${message}`));
+  }
+}
+
+export default CommandSay;

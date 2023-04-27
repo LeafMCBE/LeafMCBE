fde0717f37355ce6008b4f1f39ffb35771ff60de
hvlxh <monsta22019@gmail.com>
2023-04-27 23:17:46 +0530

    Updated version to `1.19.80`

--- source/server/Validator.ts
+++ source/server/Validator.ts
@@ -2,29 +2,34 @@ import { mkdir, writeFile } from 'node:fs/promises'
 import { existsSync } from 'node:fs'
 
 const writeConfig = async () => {
-  await writeFile('./leaf/config.yml', `Server:
+  await writeFile(
+    './leaf/config.yml',
+    `Server:
   host: 0.0.0.0
   port: 19132
   motd: "Dedicated LeafMCBE Server"
-  version: 1.19.50
+  version: 1.19.80
 LeafMCBE:
-  debug: true
-  showDateOnLogging: true
-  doNotCrashOnError: true
-World:
-  gamemode: "creative" # survival / creative / adventure
-  dimension: "flat" # overworld / flat / nether / end / void
-  biome: "plains"`)
+  Terminal:
+    debug: true
+    showDateOnLogging: true
+    doNotCrashOnError: true`
+  )
 }
 
 const validate = async () => {
-  if (!existsSync('./leaf')) { await mkdir('./leaf') }
-
-  if (!existsSync('./leaf/config.yml')) { await writeConfig() }
-
-  if (!existsSync('./leaf/plugins')) { await mkdir('./leaf/plugins') }
-
-  if (!existsSync('./leaf/players')) { await mkdir('./leaf/players') }
+  if (!existsSync('./leaf')) {
+    await mkdir('./leaf')
+  }
+  if (!existsSync('./leaf/config.yml')) {
+    await writeConfig()
+  }
+  if (!existsSync('./leaf/plugins')) {
+    await mkdir('./leaf/plugins')
+  }
+  if (!existsSync('./leaf/players')) {
+    await mkdir('./leaf/players')
+  }
 }
 
 export default validate

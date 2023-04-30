ece8ca887fae3fa5211708cb0a7dd2ec50740b11
hvlxh <monsta22019@gmail.com>
2023-04-30 16:17:04 +0530

    Logger: fixes some issue (there is still some)

--- source/packets/handlers/PacketText.ts
+++ source/packets/handlers/PacketText.ts
@@ -5,19 +5,9 @@ import Player from '../../api/Player'
 
 const PacketText = (server: Server, client: any, packet: any) => {
   const maybe = () => {
-    const txt = new Text()
-      .setMessage(
-        `<${client.getUserData().displayName}> ${packet.data.params.message}`
-      )
-      .setType(TextType.Chat)
-      .setNeedsTranslation(false)
-      .setSourceName('')
-      .setPlatformChatId('')
-      .setXuid('')
-
-    for (const player of server.players) {
-      txt.execute(player.client)
-    }
+    server.broadcast(
+      `<${client.getUserData().displayName}> ${packet.data.params.message}`
+    )
   }
 
   server.plugins.do('onPlayerChat', maybe, new Player(client))
